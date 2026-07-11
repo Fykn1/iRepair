49f27cc558845e81f4328882324849c02799f87e
Fykn <fernandokodagawa@gmail.com>
2026-07-10 23:35:35 -0300

    feat: Termina de configurar Vite React files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-function App() {
+import { Header } from "./components/Header"
+
+export function App() {
   return (
-    <h1 className="text-3x1 font-bold underline">
-      Hello World
-    </h1>
+    <Header />
   )
 }
