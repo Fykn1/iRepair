ee8015fdf8b610b508a771bf0c30013039ba10d1
Fykn <fernandokodagawa@gmail.com>
2026-07-17 04:37:19 -0300

    refactor: transforma Header em arrow function

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
-export function Header() {
+const Header = () => {
   return (
     <header className="bg-zinc-800 text-white p-4">
       <h1>iJunior Task Board</h1>
     </header>
   );
-}
+};
+
+export default Header;
