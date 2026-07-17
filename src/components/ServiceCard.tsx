edda72d3b4fc8e6fe998bb6ea55673e25c5d7439
Fykn <fernandokodagawa@gmail.com>
2026-07-17 05:40:38 -0300

    refactor: muda ServiceCard para atender especificações

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,27 +1,33 @@
 import { useState } from "react";
 
 interface ServiceCardProps {
-  nome: string;
-  modelo: string;
-  defeito: string;
-  status: boolean;
+  id: number,
+  client_id: number,
+  device: string,
+  issue: string,
+  status: boolean,
+  created_at: string
 }
 
-export function ServiceCard({ nome, modelo, defeito, status: statusInicial }: ServiceCardProps) {
-  const [status, setStatus] = useState(statusInicial);
+const ServiceCard = ({ id, client_id, device, issue, status: inicialState, created_at }: ServiceCardProps) => {
+  const [status, setStatus] = useState(inicialState);
 
-  function mudaStatus() {
+  function changeState() {
     setStatus(!status);
   }
 
   return (
     <button 
-      onClick={mudaStatus}
+      onClick={ changeState }
       className={`${status ? "bg-green-500" : "bg-red-600"} h-36 w-72 border-3 rounded-2xl border-gray-500 grid cursor-pointer hover:opacity-90`}>
-      <p className="justify-self-start">Nome: {nome}</p>
-      <p className="justify-self-start">Modelo: {modelo}</p>
-      <p className="justify-self-start">Defeito: {defeito}</p>
+      <p className="justify-self-start">Id: {id}</p>
+      <p className="justify-self-start">Id do Cliente: {client_id}</p>    
+      <p className="justify-self-start">Modelo: {device}</p>
+      <p className="justify-self-start">Defeito: {issue}</p>
       <p className="justify-self-start">Status: {status ? "Finalizado" : "Aberto"}</p>
+      <p className="justify-self-start">Data de Criação: {created_at}</p>
     </button>
   );
 }
+
+export default ServiceCard;
