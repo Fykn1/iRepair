ffbafa689cd1ee51e412e2186c14ba0951272e78
Fykn <fernandokodagawa@gmail.com>
2026-08-10 16:44:00 -0300

    feat: adiciona clientRoutes ao index.ts

--- api/src/config/expressConfig.ts
+++ api/src/config/expressConfig.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { userRoutes } from '../routes/userRoute';
+import { clientRoutes } from '../routes/clientRoute';
 
 const app = express();
 app.use(express.json());
 app.use('/users', userRoutes);
+app.use('/clients', clientRoutes);
 
 export { app };
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { userRoutes } from './routes/userRoute';
+import { clientRoutes } from './routes/clientRoute';
 import { app } from './config/expressConfig';
 
 const PORT = 3333;
 
 app.use(express.json());
 app.use(userRoutes);
+app.use(clientRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
