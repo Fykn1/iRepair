d7e06897122cd7c8ef32c2312e56ec929e89febb
Fykn <fernandokodagawa@gmail.com>
2026-08-09 21:20:22 -0300

    refactor: move front-end para /client (monorepo setup)

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router';
+import DashboardPage from './pages/DashboardPage';
+import ClientsPage from './pages/ClientsPage';
+import ServiceOrdersPage from './pages/ServiceOrdersPage';
+import MainLayout from "./components/MainLayout";
+
+const App = () => {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/clients" element={<ClientsPage />} />
+            <Route path="/service-orders" element={<ServiceOrdersPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
---
+++ client/src/pages/DashboardPage.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import { api } from '../services/api';
+import ServiceCard from '../components/ServiceCard';
+
+interface Service {
+  id: number,
+  client_id: number,
+  device: string,
+  issue: string,
+  status: boolean,
+  created_at: string
+}
+
+const DashboardPage = () => {
+  const [services, setServices] = useState<Service[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await api.get('/service-orders');
+        setServices(response.data);
+      } catch (e) {
+        console.error(e);
+        setError('Não foi possível carregar as ordens de serviço.');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchProducts();
+  }, []);
+
+  if (isLoading) return <p>Carregando...</p>;
+  if (error) return <p className="text-red-500">{error}</p>;
+
+  return (
+    <>
+      <p>Dashboard</p>
+      {services.map((service) => (
+        <ServiceCard
+          id={service.id}
+          client_id={service.client_id}
+          device={service.device}
+          issue={service.issue}
+          status={service.status}
+          created_at={service.created_at}
+        />
+      ))}
+    </>
+  );
+};
+
+export default DashboardPage;
