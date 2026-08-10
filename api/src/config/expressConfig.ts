b7ec1bdd7c32aae92ceaff1768f5674aeded2233
Fykn <fernandokodagawa@gmail.com>
2026-08-10 16:24:13 -0300

    feat: implementa userRoutes no expressConfig

--- api/src/config/expressConfig.ts
+++ api/src/config/expressConfig.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import { userRoutes } from '../routes/userRoute';
 
 const app = express();
 app.use(express.json());
+app.use('/users', userRoutes);
 
 export { app };
