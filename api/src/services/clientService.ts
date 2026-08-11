4b01ece7e4f3a4414de2abd90dd668313410a0ce
Fykn <fernandokodagawa@gmail.com>
2026-08-11 13:37:41 -0300

    refactor: arruma indentação

--- api/src/services/clientService.ts
+++ api/src/services/clientService.ts
@@ -4,81 +4,81 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client';
 
 class ClientService {
   async create({ name, phone, email }: Omit<Client, 'id' | 'createdAt' | 'serviceOrders'>) {
-      if (!name || !phone || !email) {
+    if (!name || !phone || !email) {
       throw new Error("Cliente deve ter nome, telefone e email");
     }
-  
-      const newClient = await prisma.client.create({
-        data: {
-          name,
-          phone,
-          email
-        }
-      });
-  
-      return newClient;
+
+    const newClient = await prisma.client.create({
+      data: {
+        name,
+        phone,
+        email
+      }
+    });
+
+    return newClient;
+  }
+
+  async getAll() {
+    return await prisma.client.findMany({
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        phone: true,
+        createdAt: true,
+      }
+    });
+  }
+
+  async getById(id: string) {
+    const client = await prisma.client.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        phone: true,
+        createdAt: true,
+      }
+    });
+
+    if (!client) {
+      throw new Error("404: Client not found");
     }
-  
-    async getAll() {
-      return await prisma.client.findMany({
-        select: {
-          id: true,
-          email: true,
-          name: true,
-          phone: true,
-          createdAt: true,
-        }
-      });
-    }
-  
-    async getById(id: string) {
-      const client = await prisma.client.findUnique({
+
+    return client;
+  }
+
+  async update(id: string, data: Partial<Omit<Client, 'id' | 'createdAt' | 'serviceOrders'>>) {
+    try {
+      const client = await prisma.client.update({
         where: { id },
-        select: {
-          id: true,
-          email: true,
-          name: true,
-          phone: true,
-          createdAt: true,
-        }
+        data
       });
-  
-      if (!client) {
-        throw new Error("404: Client not found");
-      }
-  
+
       return client;
-    }
-  
-    async update(id: string, data: Partial<Omit<Client, 'id' | 'createdAt' | 'serviceOrders'>>) {
-      try {
-        const client = await prisma.client.update({
-          where: { id },
-          data
-        });
-  
-        return client;
-      
-      } catch (error) {
-        if (error instanceof PrismaClientKnownRequestError && error.code === 'P2025') {
-          throw new Error('404: Client not found');
-        }
-  
-        throw error;
+    
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError && error.code === 'P2025') {
+        throw new Error('404: Client not found');
       }
-    }
-  
-    async delete(id: string) {
-      try {
-        await prisma.client.delete({ where: { id } });
-      
-      } catch (error) {
-        if (error instanceof PrismaClientKnownRequestError && error.code === 'P2025') {
-          throw new Error('404: Client not found');
-        }
-        throw error;
-      } 
+
+      throw error;
     }
   }
-  
-  export { ClientService };
+
+  async delete(id: string) {
+    try {
+      await prisma.client.delete({ where: { id } });
+    
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError && error.code === 'P2025') {
+        throw new Error('404: Client not found');
+      }
+      throw error;
+    } 
+  }
+}
+
+export { ClientService };
