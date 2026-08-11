220f2527ae2ce05bae05201c409c29346d3e5061
Fykn <fernandokodagawa@gmail.com>
2026-08-11 13:36:31 -0300

    feat: adiciona serviceOrderRoutes ao index.ts

--- api/src/config/expressConfig.ts
+++ api/src/config/expressConfig.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { userRoutes } from '../routes/userRoute';
 import { clientRoutes } from '../routes/clientRoute';
+import { serviceOrderRoutes } from '../routes/serviceOrderRoute';
 
 const app = express();
 app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/clients', clientRoutes);
+app.use('/service-orders', serviceOrderRoutes);
 
 export { app };
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { userRoutes } from './routes/userRoute';
 import { clientRoutes } from './routes/clientRoute';
+import { serviceOrderRoutes } from './routes/serviceOrderRoute';
 import { app } from './config/expressConfig';
 
 const PORT = 3333;
@@ -8,6 +9,7 @@ const PORT = 3333;
 app.use(express.json());
 app.use(userRoutes);
 app.use(clientRoutes);
+app.use(serviceOrderRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
