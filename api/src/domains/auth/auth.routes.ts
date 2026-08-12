1d8a959acdc9eaf356128ecf9b452760350b6197
Fykn <fernandokodagawa@gmail.com>
2026-08-12 09:27:03 -0300

    feat: adiciona auth.me

--- api/src/domains/auth/auth.routes.ts
+++ api/src/domains/auth/auth.routes.ts
@@ -10,5 +10,6 @@ authRoutes.use(authMiddleware)
 authRoutes.post('/register', authController.register.bind(authController))
 authRoutes.post('/login',    authController.login.bind(authController))
 authRoutes.post('/logout',   authController.logout.bind(authController))
+authRoutes.get('/me',        authMiddleware, authController.me.bind(authController))
 
 export { authRoutes }
