7eb5a53dd1a8284edc3aa0b42c0b1ae48fd46d12
Fykn <fernandokodagawa@gmail.com>
2026-08-12 10:09:17 -0300

    fix: aplica middleware apenas em /logout e /me

--- api/src/domains/auth/auth.routes.ts
+++ api/src/domains/auth/auth.routes.ts
@@ -7,7 +7,7 @@ const authController = new AuthController()
 
 authRoutes.post('/register', authController.register.bind(authController))
 authRoutes.post('/login',    authController.login.bind(authController))
-authRoutes.post('/logout',   authController.logout.bind(authController))
+authRoutes.post('/logout',   authMiddleware, authController.logout.bind(authController))
 authRoutes.get('/me',        authMiddleware, authController.me.bind(authController))
 
 export { authRoutes }
