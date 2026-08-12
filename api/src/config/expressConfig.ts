bd18103aa321eb6e9c3089fc8be0e787f86e3fb8
Fykn <fernandokodagawa@gmail.com>
2026-08-12 08:43:56 -0300

    feat: registra cookie-parser no expressConfig

--- api/src/config/expressConfig.ts
+++ api/src/config/expressConfig.ts
@@ -3,6 +3,7 @@ import { userRoutes } from '../routes/userRoute';
 import { clientRoutes } from '../routes/clientRoute';
 import { serviceOrderRoutes } from '../routes/serviceOrderRoute';
 import { authRoutes } from '../domains/auth/auth.routes';
+import cookieParser from 'cookie-parser'
 
 const app = express();
 app.use(express.json());
@@ -10,5 +11,6 @@ app.use('/users', userRoutes);
 app.use('/clients', clientRoutes);
 app.use('/service-orders', serviceOrderRoutes);
 app.use('/auth', authRoutes);
+app.use(cookieParser())
 
 export { app };
