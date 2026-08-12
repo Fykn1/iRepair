b3e177a49854ddeb18b583d1aa2b0f33ddf158d9
Fykn <fernandokodagawa@gmail.com>
2026-08-12 15:47:06 -0300

    refactor: deleta files duplicados

--- api/src/models/userModel.ts
+++
@@ -1,9 +0,0 @@
-import type { ServiceOrder } from "./serviceOrderModel";
-
-export interface User {
-  id: string;
-  email: string;
-  password: string;
-  createdAt: string;
-  serviceOrders?: ServiceOrder[]
-}
