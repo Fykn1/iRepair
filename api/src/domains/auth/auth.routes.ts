fb6e9b76efcf64b96b3b75fe815a1710abf81213
Fykn <fernandokodagawa@gmail.com>
2026-08-12 08:59:59 -0300

    feat: adiciona authMiddleware a authRoutes

--- api/src/domains/auth/auth.routes.ts
+++ api/src/domains/auth/auth.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express'
 import { AuthController } from './auth.controller'
+import { authMiddleware } from '../../middlewares/auth.middleware'
 
 const authRoutes = Router()
 const authController = new AuthController()
+
+authRoutes.use(authMiddleware)
 
 authRoutes.post('/register', authController.register.bind(authController))
 authRoutes.post('/login',    authController.login.bind(authController))
