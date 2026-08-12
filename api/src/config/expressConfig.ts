7e5a43983e5264cda929be1c81d65a632d2e5cac
Fykn <fernandokodagawa@gmail.com>
2026-08-12 09:13:44 -0300

    refactor: limpa o código em expressConfig e index.ts

--- api/src/config/expressConfig.ts
+++ api/src/config/expressConfig.ts
@@ -1,21 +1,24 @@
 import express from 'express';
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
 import { userRoutes } from '../routes/userRoute';
 import { clientRoutes } from '../routes/clientRoute';
 import { serviceOrderRoutes } from '../routes/serviceOrderRoute';
 import { authRoutes } from '../domains/auth/auth.routes';
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
 
 const app = express();
+
 app.use(express.json());
-app.use('/users', userRoutes);
-app.use('/clients', clientRoutes);
-app.use('/service-orders', serviceOrderRoutes);
-app.use('/auth', authRoutes);
 app.use(cookieParser())
 app.use(cors({
   origin: 'http://localhost:5173/',
   credentials: true
 }));
+
+app.use('/users', userRoutes);
+app.use('/clients', clientRoutes);
+app.use('/service-orders', serviceOrderRoutes);
+app.use('/auth', authRoutes);
 
 export { app };
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,23 +1,15 @@
-import 'dotenv/config'
-  if (!process.env.JWT_SECRET) {
-    throw new Error('JWT_SECRET não definido nas variáveis de ambiente')
-  }
-  if (!process.env.DATABASE_URL) {
-    throw new Error('DATABASE_URL não definido nas variáveis de ambiente')
-  }
+import 'dotenv/config';
 
-import express from 'express';
-import { userRoutes } from './routes/userRoute';
-import { clientRoutes } from './routes/clientRoute';
-import { serviceOrderRoutes } from './routes/serviceOrderRoute';
+if (!process.env.JWT_SECRET) {
+  throw new Error('JWT_SECRET não definido nas variáveis de ambiente');
+}
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL não definido nas variáveis de ambiente');
+}
+
 import { app } from './config/expressConfig';
 
 const PORT = 3333;
-
-app.use(express.json());
-app.use(userRoutes);
-app.use(clientRoutes);
-app.use(serviceOrderRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
