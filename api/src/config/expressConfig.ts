991c163afa5b43c98bd3eab0f77a2bd5c2258596
Fykn <fernandokodagawa@gmail.com>
2026-08-12 09:05:30 -0300

    feat: registra cors no expressConfig

--- api/src/config/expressConfig.ts
+++ api/src/config/expressConfig.ts
@@ -4,6 +4,7 @@ import { clientRoutes } from '../routes/clientRoute';
 import { serviceOrderRoutes } from '../routes/serviceOrderRoute';
 import { authRoutes } from '../domains/auth/auth.routes';
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 const app = express();
 app.use(express.json());
@@ -12,5 +13,9 @@ app.use('/clients', clientRoutes);
 app.use('/service-orders', serviceOrderRoutes);
 app.use('/auth', authRoutes);
 app.use(cookieParser())
+app.use(cors({
+  origin: 'http://localhost:5173/',
+  credentials: true
+}));
 
 export { app };
