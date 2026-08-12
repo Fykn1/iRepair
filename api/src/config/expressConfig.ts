f27e1d26dcb2f57ed79b3e6d07126f4f33d083a4
Fykn <fernandokodagawa@gmail.com>
2026-08-11 22:25:21 -0300

    feat: registra authRoute no expressConfig

--- api/src/config/expressConfig.ts
+++ api/src/config/expressConfig.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { userRoutes } from '../routes/userRoute';
 import { clientRoutes } from '../routes/clientRoute';
 import { serviceOrderRoutes } from '../routes/serviceOrderRoute';
+import { authRoutes } from '../domains/auth/auth.routes';
 
 const app = express();
 app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/clients', clientRoutes);
 app.use('/service-orders', serviceOrderRoutes);
+app.use('/auth', authRoutes);
 
 export { app };
